a519000238a6ed00f76c475db35509a60c26f40e
nviettien191 <n.viettien191655320@gmail.com>
2017-08-05 16:47:06 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 apt-get update && apt-get -y upgrade && apt-get -y autoremove
-apt-get install libcurl4-openssl-dev git -y
+apt-get install automake -y
+apt-get install libjansson-dev -y
+apt-get install libcurl4-openssl-dev -y
 apt-get install build-essential -y
-apt-get install autotools-dev autoconf -y 
 apt-get install libcurl3 libcurl4-gnutls-dev -y
-git clone https://github.com/wolf9466/cpuminer-multi && cd cpuminer-multi && ./autogen.sh
+git clone https://github.com/OhGodAPet/cpuminer-multi.git && cd cpuminer-multi && ./autogen.sh
 CFLAGS="-march=native" ./configure
 make
+./minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u nhac.tien1988@gmail.com -p x
