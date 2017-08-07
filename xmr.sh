dc8bb8388ddcb2612d85cb519bb51be88be42744
nviettien191 <n.viettien191655320@gmail.com>
2017-08-07 22:45:45 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -5,7 +5,8 @@ apt-get install libjansson-dev -y
 apt-get install libcurl4-openssl-dev -y
 apt-get install build-essential -y
 apt-get install libcurl3 libcurl4-gnutls-dev -y
+sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 6))
 git clone https://github.com/OhGodAPet/cpuminer-multi.git && cd cpuminer-multi && ./autogen.sh
 CFLAGS="-march=native" ./configure
 make
-./minerd --threads=4 -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u nhac.tien1988@gmail.com -p x
+./minerd --threads=6 -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u nhac.tien1988@gmail.com -p x
